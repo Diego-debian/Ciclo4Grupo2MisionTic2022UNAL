59b4751db40b58fee312446caf8156f5060ba77b
Diego-debian <diegoestudianteud1@gmail.com>
2022-08-19 20:06:37 -0500

    Archivo ejemplo Mongo académico

---
+++ MongoDBEjemplos/ejemploAcademicoMongo.js
@@ -0,0 +1,21 @@
+/**
+ * Insertar uno
+ */
+ db.departamento.insertOne({"id":1, "nombre_dpto":"Física"})
+ /**
+  * Insertar muchos
+  */
+ db.departamento.insertMany([{"id":2, "nombre_dpto":"Física"},
+ {"id":3, "nombre_dpto":"Química"},
+ {"id":4, "nombre_dpto":"Ingeniería"},
+ {"id":5, "nombre_dpto":"Médicina"},
+ {"id":6, "nombre_dpto":"Derecho"}])
+ /**
+  * Eliminar
+  */
+ db.departamento.remove({"id":1})
+ 
+ /**
+  * Consultas condicionadas
+  */
+ db.departamento.find({"nombre_dpto":"Física"})
