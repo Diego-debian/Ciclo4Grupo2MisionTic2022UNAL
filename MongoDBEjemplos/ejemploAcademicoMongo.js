fbe0345b40b6383626cf7540ae4cdef739698b22
Diego-debian <diegoestudianteud1@gmail.com>
2022-08-19 21:07:31 -0500

    Archivo ejemplo Mongo académico

--- MongoDBEjemplos/ejemploAcademicoMongo.js
+++ MongoDBEjemplos/ejemploAcademicoMongo.js
@@ -14,6 +14,7 @@
   * Eliminar
   */
  db.departamento.remove({"id":1})
+ db.materia.remove({"id":ObjectId("63003b5d7c49476cb489c2b7") })
  
  /**
   * Consultas condicionadas
@@ -36,4 +37,45 @@
  /**
   * Ciclos for => ForEach
   */
- 
+ db.departamento.find({},{"_id":0,"id":0}).sort({"nombre_dpto":1}).forEach(function(dpto)
+ {
+    print("Nombre del departamento: "+ dpto.nombre_dpto)
+ })
+ /**
+  * Llenar la colección materia
+  */
+
+ db.materia.insertMany([
+    {"id":1,"nombre":"Algebra Lineal","creditos":20},
+    {"id":2,"nombre":"Derecho Internacional","creditos":25},
+    {"id":3,"nombre":"Mecánica Cúantica","creditos":20},
+    {"id":4,"nombre":"Ánatomia","creditos":20},
+    {"id":5,"nombre":"Derecho civil","creditos":20},
+ ])
+
+ /**
+  * Contar documentos
+  */
+  db.materia.count()
+
+  /**
+   * Actualizaciones
+   */
+   db.materia.update({"id":1},{ "id" : 1, "nombre" : "Algebra Lineal", "creditos" : 25})
+   db.materia.update({"id":1},{ "creditos" : 25},{ upsert: true})
+   db.materia.update({"creditos" : 25},{ "id" : 1, "nombre" : "Algebra Lineal", "creditos" : 25}, ,{ upsert: true}))
+   db.materia.update({"id":1},{$set:{"creditos":25}},{ upsert: true}))
+   db.materia.update({"id":1},{$rename:{"name":"nombre"}},{ upsert: true})
+
+   /**
+    * Base relacionada
+    */
+
+   db.materia.aggregate({
+    $lookup:{
+        from:'departamento',
+        localField:'nombre_dpto',
+        foreignField:'nombre_dpto',
+        as:'departamento'
+    }
+   })
