bb56a494ebaf5a45f4c3968c5af3079a7f935ade
diego-debian <profediegoparra01@gmail.com>
2022-09-15 20:05:19 -0500

    Terminados los endpoints de partidos

--- ApiGateway/main.py
+++ ApiGateway/main.py
@@ -75,8 +75,8 @@ def validarPermiso(endPoint, metodo, idRol):
         pass
     return tienePermiso
 
-
-###############################################################
+###########################################################
+################# ENDPOINTS DE MESAS ######################
 @app.route("/mesas", methods=['GET'])
 def getMesas():
     headers = {"Content-Type": "application/json; charset=utf-8"}
@@ -118,6 +118,56 @@ def eliminarMesa(id):
     response = requests.delete(url, headers=headers)
     json = response.json()
     return jsonify(json)
+
+###########################################################
+###              ENDPOINTS DE PARTIDOS                  ###
+###########################################################
+@app.route("/partidos", methods=['GET'])
+def getPartidos():
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votes"]+'/partidos'
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+
+@app.route("/partidos", methods=['POST'])
+def crearPartido():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votes"]+'/partidos'
+    response = requests.post(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=['GET'])
+def getPartido(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votes"]+'/partidos/'+id
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=['PUT'])
+def modificarPartido(id):
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votes"]+'/partidos/'+id
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=['DELETE'])
+def eliminarPartido(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votes"]+'/partidos/'+id
+    response = requests.delete(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+#########################################################
+####          TEST O PRUEBA DE SERVICIO              ####
+#########################################################
 
 @app.route("/", methods=['GET'])
 def test():
